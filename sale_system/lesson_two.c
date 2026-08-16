a9700badf6d75a7f2cea69156856255f119e2405
tech-fts <zwe11398@gmail.com>
2026-08-16 10:42:13 +0630

    add type check for integer

--- sale_system/lesson_two.c
+++ sale_system/lesson_two.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
+int check_type(const char *format, void *address){
+    while(scanf(format, address) != 1){ //need to check 
+        printf("Invalid input. Please enter numbers only. \n");
+
+        while(getchar() != '\n');
+        printf("Try again...");
+    }
+
+    while(getchar() != '\n');
+    return 1;
+}
+
 char get_customerinfo(char *name, int *phone_number, char *shopname, char *address){
     printf("Enter your name: ");
     scanf("%s", name);
+    while(getchar() != '\n');
+    
     printf("Enter your phone number: ");
-    scanf("%d", &phone_number);
+    check_type("%d", phone_number);
+
     printf("Enter your shop name: ");
     scanf("%s", shopname);
     printf("Enter your address: ");
@@ -16,9 +31,23 @@ char get_saleitems(char *item_name, int *item_quantity, float *item_price){
     printf("Enter item name: ");
     scanf("%s", item_name);
     printf("Enter item quantity: ");
-    scanf("%d", &item_quantity);
+
+    //datatype check
+    while (scanf("%d", item_quantity) != 1){
+        printf("Invalid Input. Please enter number only. \n");
+        printf("Enter item quantity: ");
+
+        while(getchar() != '\n');
+    }
     printf("Enter item price: ");
-    scanf("%f", &item_price);
+
+    //datatype check
+    while (scanf("%f", item_price) != 1){
+        printf("Invalid Input. Please enter number only. \n");
+        printf("Enter item price: ");
+
+        while(getchar() != '\n');
+    }
     return 0;
 }
 
@@ -30,8 +59,7 @@ int mainaction(){
     scanf("%d", &action);
 
     while (action != 1){
-        printf("add customerinfo\n");
-        scanf("%d", &action);
+        return 0;
     }
 
     printf("continue to customer name data... \n");
@@ -39,8 +67,13 @@ int mainaction(){
 }
 
 int saleaction(){
+    int sal_action = 0;
     printf("1. Add Sale Items\n");
-    return 0;
+    scanf("%d", &sal_action);
+    if (sal_action != 1){
+        return 0;
+    }
+    return 1;
 }
 
 int main(){
@@ -65,10 +98,16 @@ int main(){
             printf("Item Name: %s\n", item_name);
             printf("Item Quantity: %d\n", item_quantity);
             printf("Item Price: %.2f\n", item_price);
+            return 0;
         }
 
         // with customerinfo
         get_customerinfo(name, &phone_number, shopname, address);
+
+        int check_sale = saleaction();
+        if (check_sale != 1){
+            return 0;
+        }
 
         //get saleitems() and sales()
         get_saleitems(item_name, &item_quantity, &item_price);
