5c785edfc467a6baf4da848b23b927f537d25adc
tech-fts <zwe11398@gmail.com>
2026-08-25 19:06:44 +0630

    calculated sonar range

--- Grid_Mapping_Simulator/lesson_one.c
+++ Grid_Mapping_Simulator/lesson_one.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 #define GRID_SIZE           15
 #define NUM_SENSORS         8
@@ -25,15 +26,44 @@ int robot_internal_map[GRID_SIZE][GRID_SIZE];
 
 void setup_environment(void){
     for(int y =0; y< GRID_SIZE; y++){
-        printf("%d\n", y);
-        for(int x=0; x< GRID_SIZE; x++){
-            printf("%d\n", x);
 
+        for(int x=0; x< GRID_SIZE; x++){
+
+            //build outer boundary wall
             if(x == 0 || x == GRID_SIZE -1 || y == 0 || y == GRID_SIZE - 1){
                 real_world_room[y][x] = CELL_WALL;
-                printf("wall creation %d", CELL_WALL);
             }
+            // created internal obstacle
+            else if(x == 4 && y == 4){
+                real_world_room[y][x] = CELL_WALL;
+            }
+            else{
+                real_world_room[y][x] = CELL_FREE;
+            }
+
+            robot_internal_map[y][x] = CELL_UNKNOWN;
         }
+    }
+}
+
+float simulate_sonar_range(float angle){
+    for(float dist = 0.5f; dist < MAX_SORNER_RANGE; dist += 0.5f){
+        int check_x = robot_x + (int)roundf(dist * cosf(angle));
+        int check_y = robot_y + (int)roundf(dist * sinf(angle));
+
+        if(check_x >= 0 && check_x < GRID_SIZE && check_y >= 0 && check_y < GRID_SIZE){
+            if(real_world_room[check_y][check_x] == CELL_WALL){
+                return dist;
+            }
+        }   
+    }
+    return MAX_SORNER_RANGE;
+}
+
+void  mapping_algorithms(void){
+    for(int i = 0; i < NUM_SENSORS; i++){
+        sonar_reading[i] = simulate_sonar_range(sensor_headings[i]);
+        printf("%.2f\n",sonar_reading[i]);
     }
 }
 
@@ -41,5 +71,6 @@ void setup_environment(void){
 int main(){
     printf("This is start of the project...\n");
     setup_environment();
+    mapping_algorithms();
     return 0;
 }
