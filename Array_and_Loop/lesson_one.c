95c6a8230abbaf28d97e410a3c5fe6f8144a9793
tech-fts <zwe11398@gmail.com>
2026-08-29 14:11:52 +0630

    memory allocation added for array size

--- Array_and_Loop/lesson_one.c
+++ Array_and_Loop/lesson_one.c
@@ -1,13 +1,55 @@
 #include <stdio.h>
-#define size 5
+#include <stdlib.h>
 
-void user_input(){
+int user_input(){
     int numbers;
-    printf("Please enter number that you want to add:");
-    scanf("%d", &numbers);
+    printf("Please enter number that you want to add: ");
+    
+    while (scanf("%d", &numbers) != 1)
+    {
+        printf("Invalid Input: Please try again: ");
+        while (getchar() != '\n');
+    }
+   return numbers; 
+}
+
+void array_adding(int numbers[], const int x){
+    int user_numbers;
+    printf("Enter numbers for array: ");
+    while( scanf("%d", &user_numbers) != 1){
+        printf("Invalid Input: Please try again: ");
+        while (getchar() != '\n');
+    }
+
+    numbers[x] = user_numbers;
 }
 
 int main(){
-    int numbers[size];
+    int count;
+    int x = 0; //assigned value first
+
+    count = user_input();
+
+    int *numbers = (int *)malloc(count * sizeof(int));
+    if (numbers == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    printf("numbers of user wanted: %d\n ", count);
+
+    while(x < count){ 
+        printf("numbers:%d\n", x);
+
+        array_adding(numbers, x);
+
+        printf("successful\n");
+        x++;
+    }
+
+    for(int i =0; i < count; i++){
+        printf("array %d\n",numbers[i]);
+    }
+
     return 0;
 }
