d7ce6aecb8125120676fe6466c29f4d42a16232d
tech-fts <zwe11398@gmail.com>
2026-08-29 17:40:48 +0630

    loopaing and array lesson done

--- Array_and_Loop/lesson_one.c
+++ Array_and_Loop/lesson_one.c
@@ -16,7 +16,7 @@ int user_input(){
 void array_adding(int numbers[], const int x){
     int user_numbers;
     printf("Enter numbers for array: ");
-    while( scanf("%d", &user_numbers) != 1){
+    while( scanf("%d", &user_numbers) != 1 && user_numbers > 1){
         printf("Invalid Input: Please try again: ");
         while (getchar() != '\n');
     }
@@ -27,28 +27,88 @@ void array_adding(int numbers[], const int x){
 int main(){
     int count;
     int x = 0; //assigned value first
+    int *numbers = NULL;
 
-    count = user_input();
+    while(1){
+        int user_opt;
+        
+    
+        printf("Please enter option: \n");
+        printf("1. array added: \n");
+        printf("2. array element delete: \n");
+        printf("3. exit\n");
 
-    int *numbers = (int *)malloc(count * sizeof(int));
-    if (numbers == NULL) {
-        printf("Memory allocation failed!\n");
-        return 1;
-    }
+        while (scanf("%d", &user_opt) != 1)
+        {
+            printf("Invalid value.Please enter again: ");
+            while(getchar() != '\n');
+        }
 
-    printf("numbers of user wanted: %d\n ", count);
+        if (user_opt == 1){
+            count = user_input();
 
-    while(x < count){ 
-        printf("numbers:%d\n", x);
+            numbers = (int *)malloc(count * sizeof(int));
+            if (numbers == NULL) {
+                printf("Memory allocation failed!\n");
+                return 1;
+            }
 
-        array_adding(numbers, x);
+            printf("numbers of user wanted: %d\n ", count);
+            int x = 0;
+            while(x < count){ // this condition check user want to add element to array
+                printf("numbers:%d\n", x);
 
-        printf("successful\n");
-        x++;
-    }
+                array_adding(numbers, x);
 
-    for(int i =0; i < count; i++){
-        printf("array %d\n",numbers[i]);
+                printf("successful\n");
+                x++;
+            }
+
+            for(int i =0; i < count; i++){
+                printf("array %d\n",numbers[i]);
+            }
+        }else if(user_opt == 2){
+            if (count == 0 || numbers == NULL){
+                printf("There is no data to delete inside this array!\n");
+                continue; // Return to main menu instead of exiting program with return 0
+            }
+
+            int delete_element;
+            printf("Enter number that you want to delete: ");
+            while (scanf("%d", &delete_element) != 1) {
+                printf("Invalid Input. Try again: ");
+                while (getchar() != '\n');
+            }
+
+            int found_index = -1;
+            for(int i = 0; i < count; i++){
+                if(numbers[i] == delete_element){
+                    found_index = i;
+                    break;
+                }
+            }
+
+            if (found_index != -1) {
+                // Shift elements left to overwrite the target index
+                for (int i = found_index; i < count - 1; i++) {
+                    numbers[i] = numbers[i + 1];
+                }
+                count--; // Reduce count by 1 ONLY ONCE
+                printf("Successfully deleted %d\n", delete_element);
+            } else {
+                printf("Number %d not found in array.\n", delete_element);
+            }
+
+            printf("\nUpdated array:\n");
+            for(int i = 0; i < count; i++){
+                printf("numbers[%d] = %d\n", i, numbers[i]);
+            }
+
+        } else if (user_opt == 3) {
+            free(numbers);
+            printf("Exiting program.\n");
+            break;
+        }
     }
 
     return 0;
