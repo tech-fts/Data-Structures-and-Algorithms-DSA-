9286c95fe46812c1cbb2575054bd65a40b1ae4ea
tech-fts <zwe11398@gmail.com>
2026-09-13 18:14:19 +0630

    updated file still need new function

--- Cooperative_Task_Scheduler_Mini_RTOS/scheduler.c
+++ Cooperative_Task_Scheduler_Mini_RTOS/scheduler.c
@@ -3,6 +3,19 @@
 
 static MinHeap_t task_heap;
 
+static void swap(Task_t *a, Task_t *b){
+    Task_t temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+static bool is_eariler(Task_t a, Task_t b){
+    if(a.next_run_time != b.next_run_time){
+        return a.next_run_time < b.next_run_time;
+    }
+    return a.priority < b.priority;
+}
+
 static void min_heap_insert(Task_t task){
     if(task_heap.size >= MAX_TASK) return;
 
@@ -15,6 +28,10 @@ static void min_heap_insert(Task_t task){
         x = (x -1)/2;
     }
 }
+
+// min_heap_extract_min(){
+
+// }
 
 void scheduler_init(void){
     task_heap.size = 0;
@@ -35,12 +52,14 @@ bool scheduler_add_task(Taskfunction taskfunc, uint32_t interval, int priority){
 }
 
 void scheduler_run(uint32_t current_time_ms){
-    for(int x=0; x<task_count; x++){
-        if(!task_list[x].is_active)continue;
+    while(task_heap.size > 0 && current_time_ms >= task_heap.array[0].next_run_time){
+        Task_t current_task = min_heap_extract_min();
 
-        if(current_time_ms - task_list[x].last_run_ms  >= task_list[x].interval_ms){
-            task_list[x].last_run_ms = current_time_ms;
-            task_list[x].execute();
+        if(current_task.execute != NULL){
+            current_task.execute();
         }
+
+        current_task.next_run_time += current_task.interval_ms;
+        min_heap_insert(current_task);
     }
 }
