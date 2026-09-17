06412e6925f55666d7e3ff3dbd6244d58c8e034d
tech-fts <zwe11398@gmail.com>
2026-09-17 20:52:55 +0630

    test function end

--- electricity_bill_calculator/test_billing.c
+++ electricity_bill_calculator/test_billing.c
@@ -6,23 +6,50 @@ struct Person test_user[2] = {
     {userName : "john_doe", password : "pass456"}
 };
 
-int test_user_login_success(void){
+void test_user_login_success(void){
     int result = login(test_user, "john_doe", "pass456");
     TEST_ASSERT_EQUAL_INT(0, result);
 }
 
-int test_user_login_success_sec(void){
+void test_user_login_success_sec(void){
     int result  = login(test_user, "john_doe", "pass456");
 
     TEST_ASSERT_EQUAL_INT(1,result);
 }
 
-int test_user_login_failed(void){
+void test_user_login_failed(void){
     int result = login(test_user, "aung aung", "password123");
 
     TEST_ASSERT_EQUAL_INT(-1, result);
 }
 
+void test_meter_usage_first(void){
+    struct Meter m  = {
+        .units_consumed = 10.1f
+    };
+    int index =0;
+
+    struct Person people = meter_usageCalculation(m, test_user, index);
+    TEST_ASSERT_EQUAL_FLOAT(50.0f, people.total_usage);
+}
+
+void test_meter_usage_zero(void){
+    struct  Meter m = 
+    {
+        .units_consumed = 0.0f
+    };
+    int index = 0;
+
+    struct Person people = meter_usageCalculation(m, test_user, index);
+    TEST_ASSERT_EQUAL_FLOAT(00.00f, people.total_usage);
+}
+
 int main(){
-    return 0;
+    UNITY_BEGIN();
+    RUN_TEST(test_user_login_success);
+    RUN_TEST(test_user_login_success_sec);
+    RUN_TEST(test_user_login_failed);
+    RUN_TEST(test_meter_usage_first);
+    RUN_TEST(test_meter_usage_zero);
+    return UNITY_END();
 }
